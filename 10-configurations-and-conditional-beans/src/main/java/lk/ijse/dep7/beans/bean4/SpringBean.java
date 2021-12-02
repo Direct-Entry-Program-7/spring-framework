cd235a6ec3315ec58c5da5ffdb3c6b829a17f0f5
Ranjith Suranga <d.m.ranjith.sura@gmail.com>
2021-12-02 12:29:09 +0530

    Add value injections module

--- 10-configurations-and-conditional-beans/src/main/java/lk/ijse/dep7/beans/bean4/SpringBean.java
+++ 10-configurations-and-conditional-beans/src/main/java/lk/ijse/dep7/beans/bean4/SpringBean.java
@@ -1,9 +1,22 @@
 package lk.ijse.dep7.beans.bean4;
 
+import org.springframework.beans.factory.annotation.Autowired;
+
+import javax.annotation.PostConstruct;
+
 public class SpringBean {
 
-    public SpringBean() {
+//    @Autowired
+    private String name;
+
+    public SpringBean(String name) {
         System.out.println(SpringBean.class.getName());
+        System.out.println(name);
     }
+
+//    @PostConstruct
+//    public void init(){
+//        System.out.println(name);
+//    }
 
 }
--- 10-configurations-and-conditional-beans/src/main/java/lk/ijse/dep7/spring/AppConfig.java
+++ 10-configurations-and-conditional-beans/src/main/java/lk/ijse/dep7/spring/AppConfig.java
@@ -1,10 +1,17 @@
 package lk.ijse.dep7.spring;
 
 import lk.ijse.dep7.beans.bean1.Test;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
 @ComponentScan(basePackageClasses = Test.class)
 @Configuration
 public class AppConfig {
+
+    @Bean
+    public String myString(){
+        return "IJSE";
+    }
+
 }
